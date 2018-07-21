5fcacc0c48e8c00b149de76f806da4d123ce5d20
B2B\dysont <tom.dyson@reedbusiness.com>
2018-07-21 15:48:58 +0100

    Updated base with doc strings

--- base.py
+++ base.py
@@ -4,21 +4,70 @@ import datetime
 import joblib
 
 
-class RedditScraper():
+class RedditScraper:
     def __init__(self, reddit_instance, subreddit_list):
+        """Scrape reddit comments about stocks
+            Parameters:
+            ----------
+            reddit_instance: PrawReddit agent.
+                Agent to use for scraping.
+
+            subreddit_list : list of string
+                list of subreddits by name that will be searched and sraped.
+        """
+
         self.reddit_instance = reddit_instance
         self.subreddits = self.subreddit_concat(subreddit_list)
+        self.stock_list = None
 
     def subreddit_concat(self, subreddit_list):
+        """Concatenate list of subreddits into one long string - how praw uses it.
+            Parameters:
+            ----------
+
+            subreddit_list : list of string
+                list of subreddits by name that will be searched and sraped.
+
+            Returns:
+            --------
+
+            string of joined list
+        """
         return '+'.join(subreddit_list)
 
     def retrieve_stock_comments(self, stock_to_search):
-        scraped_comment_list = []
-        subreddit_instance = self.reddit_instance.subreddit(self.subreddits).search(query=stock_to_search, sort='new')
+        """Retrieve comments about a stock in each subreddit.
 
+            Parameters:
+            ----------
+            stock_to_search: string.
+                string stock that praw will search
+
+            Returns:
+            --------
+            scraped_comment_list: list of dictionaries
+                dictionaries that contain information about a comment that relates to the stock
+
+        """
+        scraped_comment_list = []
+        # search through all the subreddits for a particular string
+        subreddit_instance = self.reddit_instance.subreddit(self.subreddits).search(query=stock_to_search, sort='new')
+        multi_stock_list = self.stock_list.copy()
+        multi_stock_list.remove(stock_to_search)
+
+        #loop through every submission returned in the search
         for submission in subreddit_instance:
             title = submission.title  # Output: the submission's title
+            multi_stock = False
+            #look for if people are talking about more than one stock
+            if multi_stock_list is not None:
+                for new_stock in multi_stock_list:
+                    if new_stock in title:
+                        multi_stock = True
+            #retrieve all the comments in a comment tree, will only have a few without this
             submission.comments.replace_more(limit=0)
+
+            #loop through comments on particular post
             for comment in submission.comments.list():
                 if comment.body == '[deleted]':
                     continue
@@ -27,17 +76,19 @@ class RedditScraper():
                 comment_dict['body'] = comment.body
                 comment_dict['date'] = datetime.datetime.fromtimestamp(comment.created).date()
                 comment_dict['stock'] = stock_to_search
+                comment_dict['multi_stock'] = multi_stock
                 scraped_comment_list.append(comment_dict)
-
         return scraped_comment_list
 
     def retrieve_list_comments(self, stock_list):
         com_list_o_lists = []
+        self.stock_list = stock_list
         for stock in stock_list:
             comments = self.retrieve_stock_comments(stock)
             print(stock, ':', len(comments))
-            com_list_o_lists.append(comments)
-        return [item for sublist in com_list_o_lists for item in sublist]
+            # com_list_o_lists.append(comments)
+            com_list_o_lists += comments
+        return com_list_o_lists
 
 
 if __name__ == '__main__':
@@ -66,8 +117,8 @@ if __name__ == '__main__':
                   # 'GSK',
                   # 'TTM',
                   # 'CCL',
-                  'X',
-                  'V',
+                  # 'X',
+                  # 'V',
                   # 'TXN',
                   'BA',
                   'WFC',
@@ -79,7 +130,7 @@ if __name__ == '__main__':
                   'ATVI',
                   'LMT',
                   'AMD',
-                  'F',
+                  # 'F',
                   'WMT',
                   'MCD',
                   'MU',
@@ -90,8 +141,8 @@ if __name__ == '__main__':
                   'HSBC',
                   'FB']
 
-    reddit = praw.Reddit(user_agent='your agent',
-                         client_id='your id', client_secret="your secret")
+    reddit = praw.Reddit(user_agent='Comment Extraction (by /u/Vengefulsausage1)',
+                         client_id='FLEcBnZJ-fEUqg', client_secret="f8cCHFqKQpr1wCFyEpuXr3vIvME")
 
     subreddit_list = ['stocks', ' investing', ' StockMarket', ' RobinHood', ' Stock_Picks', ' pennystocks',
                       ' portfolios', ' investing_discussion', ' finance', ' investmentclub', ' economics']
@@ -99,5 +150,5 @@ if __name__ == '__main__':
     stock_comment_list = RedditScraper(reddit, subreddit_list).retrieve_list_comments(stock_list)
     joblib.dump(stock_comment_list, 'stock_comment_list.pkl')
 
-    print(len(stock_list))
-    print(stock_list[13000])
+    print(len(stock_comment_list))
+    print(stock_comment_list[13000])
